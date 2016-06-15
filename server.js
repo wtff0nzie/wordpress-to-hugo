3f38b75a844eaaa346a205da0bb2ecaed6f0e184
Dez <dez@backpagecms.net>
2016-06-15 11:03:16 +0100

    Add license. Config now in package.json. ES2015-ify. Extract metadata for HUGO.

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-var config = require('./package.json'),
+/**
+ * @license
+ * lodash <https://lodash.com/>
+ * Copyright Dez Iddon and other contributors
+ * Released under MIT license
+ */
+
+'use strict';
+
+const config = require('./package.json').settings,
     xml2json = require('xml2json'),
     request = require('request'),
     cheerio = require('cheerio'),
     upndown = require('upndown'),
-    sitemapURL = config.sitemap,
-    fetchInterval = 3 * 1000,
-    contentSelector,
-    tagsSelector;
-
-
-contentSelector = '.section-content';
-tagsSelector  = '.post-tags a[rel=tag]';
+    url = require('url'),
+    fs = require('fs');
 
 
 // Fetch a page
-function fetchPage(URL) {
-    var exclude = [
-            '/category/',
-            '/tag/'
-        ],
-        page = {
+let fetchPage = (URL) => {
+    let page = {
             URL         : URL,
             markup      : '',
             markdown    : ''
         },
-        skip = false;
+        skip = false,
+        uri = url.parse(URL);
 
     // Is this portion of the site on the skip list?
-    exclude.forEach(function (excludeURL) {
-        if (URL.indexOf(excludeURL) > -1) {
+    config.excludePaths.forEach((excludeURL) => {
+        if (uri.path.indexOf(excludeURL) > -1) {
             skip = true;
         }
     });
@@ -40,7 +40,7 @@ function fetchPage(URL) {
     console.log('Grabbing ' + URL);
 
     // Grab page, begin the transformation (MUHAHHAHAHAHAHAHA *THUNDER CLAP*)
-    request(URL, function (err, response, body) {
+    request(URL, (err, response, body) => {
         if (err || response.statusCode !== 200) {
             return console.log(err);
         }
@@ -48,56 +48,137 @@ function fetchPage(URL) {
         page.markup = body;
         toMarkdown(page);
     });
-}
+};
 
 
 // Extract any useful metadata
-function discoverMeta(page) {
-    var $ = cheerio.load(page.markup),
+let discoverMeta = (page) => {
+    let $ = cheerio.load(page.markup),
+        pageCategories = [],
         pageTags = [];
 
-    page.content = $(contentSelector).html();
+    page.title = $(config.selectors.title).text();
+    page.published = $(config.selectors.published).attr('content');
+    page.description = $(config.selectors.description).attr('content');
 
-    $(tagsSelector).each(function(index, element) {
-        if (!element || !element.html) {
-            return;
-        }
-        pageTags.push(element.html());
+    page.content = $(config.selectors.content).html();
+
+    // Extract post specific tags
+    $(config.selectors.tags).each((index, element) => {
+        pageTags.push($(element).text());
+    });
+
+    // Extract post categories
+    $(config.selectors.categories).each((index, element) => {
+        pageCategories.push($(element).text());
     });
 
     page.tags = pageTags;
-    console.log(pageTags);
+    page.categories = pageCategories;
 
     $ = null;
-}
+};
+
+
+// Extract metadata used by Hugo static site generator
+let genHugoMeta = (page) => {
+    let hugoMeta = '---\n';
+
+    if (config.addHugoMetaData === "true") {
+        hugoMeta += 'title: "' + page.title + '"\n';
+        hugoMeta += 'description: "' + page.description + '"\n';
+        hugoMeta += 'date: "' + page.published + '"\n';
+        hugoMeta += 'categories:\n';
+        page.categories.forEach((category) => {
+            hugoMeta += '   - "' + category + '"\n';
+        });
+        hugoMeta += 'tags:\n';
+        page.tags.forEach((tag) => {
+            hugoMeta += '   - "' + tag + '"\n';
+        });
+        hugoMeta += '---\n';
+        return hugoMeta;
+    }
+    return "";
+};
 
 
 // Convert page to markdown
-function toMarkdown(page) {
-    var und = new upndown();
+let toMarkdown = (page) => {
+    let und = new upndown();
 
     discoverMeta(page);
 
-    und.convert(page.content, function(err, markdown) {
+    und.convert(page.content, (err, markdown) => {
         if (err) {
             console.log('Could not convert to MD');
             return console.err(err);
         }
-        page.markdown = markdown;
+
+        page.markdown = genHugoMeta(page) + markdown;
         saveToDisk(page);
     });
-}
+};
+
+
+// Recursively build directories
+let buildPath = (path) => {
+    let dirs = path.split('/'),
+        currentPath = './extracts';
+
+    dirs.pop();
+
+    dirs.forEach((dir) => {
+        currentPath += '/' + dir;
+
+        if (!fs.existsSync(currentPath)) {
+            fs.mkdirSync(currentPath);
+        }
+    });
+};
 
 
 // Persist markdown to disk
-function saveToDisk(page) {
-    console.log('Saved ' + page.URL);
-    console.log(page.markdown);
-}
+let saveToDisk = (page) => {
+    let uri = url.parse(page.URL),
+        path = uri.path;
+
+    if (path.slice(-1) === '/') {
+        path += 'index.md';
+    }
+
+    buildPath('md' + path);
+    buildPath('raw' + path);
+
+    fs.writeFile('./extracts/md' + path, page.markdown, (err) => {
+        if (err) {
+            console.log('Could not save ' + path);
+            console.log(err);
+        }
+    });
+
+    fs.writeFile('./extracts/raw' + path.replace('.md', '.json'), JSON.stringify(page), (err) => {
+        if (err) {
+            console.log('Could not save ' + path);
+            console.log(err);
+        }
+    });
+
+    return;
+
+    fs.writeFile('/extracts/md' + path, page.markdown, function (err){
+        if (err) {
+            console.log('Could not save ' + path);
+            console.log(err);
+        }
+    });
+};
 
 
 // Grab sitemap
-request(sitemapURL, function (err, response, body) {
+request(config.sitemap, (err, response, body) => {
+    let sitemap;
+
     if (err || response.statusCode !== 200) {
         return console.log(err);
     }
@@ -106,9 +187,11 @@ request(sitemapURL, function (err, response, body) {
     sitemap = JSON.parse(xml2json.toJson(body));
 
     // Spider links
-    sitemap.urlset.url.forEach(function (link, index) {
-        setTimeout(function () {
+    fetchPage('https://www.taxamo.com/2015-vat-changes/');
+    return;
+    sitemap.urlset.url.forEach((link, index) => {
+        setTimeout(() => {
             fetchPage(link.loc);
-        }, index * fetchInterval);
+        }, index * parseInt(config.fetchInterval, 10));
     });
 });
--- utils.js
+++
@@ -1,1 +0,0 @@
-//https://www.taxamo.com/sitemap.xml
